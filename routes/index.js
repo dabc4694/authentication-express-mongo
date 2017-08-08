e8ff82274ae79a0e0804dd74337b85c2464e5375
darshan4694 <darshan.shah4694@gmail.com>
2017-08-07 23:18:58 -0500

    Added Registration form in register.pug and rendered using index.js

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 // GET /
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   return res.render('index', { title: 'Home' });
 });
 
+// GET /register
+router.get('/register', function (req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', function (req, res, next) {
+  return res.send('Register selected!');
+});
+
 // GET /about
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   return res.render('about', { title: 'About' });
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function (req, res, next) {
   return res.render('contact', { title: 'Contact' });
 });
 
